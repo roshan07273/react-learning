ab6a6ebda948745b7839bd566f78e356ddcb2888
Roshan Raturi <roshanraturi804@gmail.com>
2024-04-07 16:29:17 +0530

    Roshan Raturi

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,50 @@
-import React from "react";
+import React, { useEffect } from "react";
 import RestaurantCard from "../components/RestaurantCard";
 import restaurantList from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 const Body = () => {
   const [listOfRestaurant, setlistOfRestaurant] = useState(restaurantList);
-  return (
+  const [searchText, setsearchText] = useState("");
+  //API calling using use effect hook
+  /*  useEffect(() => {
+  //   fetchData();
+  // }, []);
+
+  // const fetchData = async () => {
+  //   const data = await fetch(
+  //     "https://www.swiggy.com/mapi/restaurants/list/v5?lat=28.65420&lng=77.23730&collection=83667&tags=layout_CCS_Sandwiches&sortBy=&filters=&type=rcv2&offset=0&carousel=true&third_party_vendor=1"
+  //   );
+  //   const json = await data.json();
+  //   setlistOfRestaurant(json?.data?.cards[2]?.data?.data?.cards);
+  };*/
+
+  return listOfRestaurant.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setsearchText(e.target.value);
+            }}
+          />
+          <button
+            className="search-btn"
+            onClick={() => {
+              const filterData = listOfRestaurant.filter((items) => {
+                return items.data.name.toLowerCase().includes(searchText);
+              });
+              setlistOfRestaurant(filterData);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Logo from "../../assets/logo.png";
+import { useState } from "react";
 const Header = () => {
+  const [btnName, setbtnName] = useState("Login");
   return (
     <div className="header">
       <div className="logo-container">
@@ -12,6 +14,14 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              btnName === "Login" ? setbtnName("Logout") : setbtnName("Login");
+            }}
+          >
+            {btnName}
+          </button>
         </ul>
       </div>
     </div>
